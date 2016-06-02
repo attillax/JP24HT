616608ea2317e9f5e0dedf01a378eec002408c91
attillax <attillax.lab@gmail.com>
2016-06-02 22:14:51 +0200

    Add ch07 part2 solution

---
+++ ch07/Contractor.java
@@ -0,0 +1,18 @@
+public class Contractor implements Payable {
+    private String name;
+
+    public Contractor(String name) {
+        this.name = name;
+    }
+
+    public boolean increasePay(int percent) {
+        if(percent < Payable.INCREASE_CAP){
+            System.out.println("Increasing hourly rate by " +
+percent + "%. " + name);
+            return true;
+        } else {
+            System.out.println("Sorry, can't increase hourly rate by more than " + Payable.INCREASE_CAP + "%: " + name);
+            return false;
+        }
+    }
+}
---
+++ ch07/Employee.java
@@ -0,0 +1,12 @@
+public class Employee implements Payable{
+    private String name;
+
+    public Employee(String name){
+        this.name=name;
+    }
+
+    public boolean increasePay(int percent) {
+        System.out.println("Increasing salary by " + percent + "%: " + name);
+        return true;
+    }
+}
